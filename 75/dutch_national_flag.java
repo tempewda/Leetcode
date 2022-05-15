e8dfb1f7523b43bbef779c6eb9e71ba840014a25
ArrowAi <tanmoyroy1811@gmail.com>
2022-05-15 14:51:57 +0530

    updated algo

--- 75/dutch_national_flag.java
+++ 75/dutch_national_flag.java
@@ -1,7 +1,26 @@
 class Solution 
 {
-    public void sortColors(int[] nums) 
+    public int removeDuplicates(int[] nums) 
     {
-        int 
+        int ins_index = -1;
+        int n = nums.length;
+        int srch_ele = -1;
+        
+        for(int index = 0; index < n; ++index)
+        {
+            if(index == 0)
+            {
+                srch_ele = nums[index];
+                ins_index = 1;
+                continue;
+            }
+            if(nums[index] != srch_ele)    
+            {
+                srch_ele = nums[index];
+                nums[ins_index] = nums[index];
+                ++ins_index;
+            }    
+        }
+        return ins_index;
     }
 }
